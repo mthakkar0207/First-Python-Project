3e4c82772a1262027fd5e1dbaf41916dbff540dd
mthakkar0207 <mthakkar0207@gmail.com>
2025-08-09 00:57:57 -0400

    print age diff

--- first-project.py
+++ first-project.py
@@ -2,3 +2,7 @@ print("Hello! What is your name?" )
 name="Maria"
 print(f"Nice to meet you ,{name} !")
 print(" How old are you ?")
+age=45
+bot_age= 3
+age_difference= age-bot_age
+print(f"You are {age_difference} years older than me .I'm only {bot_age} years old !")
